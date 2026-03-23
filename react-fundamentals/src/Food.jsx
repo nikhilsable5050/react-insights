d506e155a4173ab2e3b07e6cb986707b4d674637
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-23 11:37:14 +0530

    add food list

---
+++ react-fundamentals/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food(){
+    const food1 = "Banana";
+    const food2 = "Apple";
+
+    return(
+        <ul>
+            <li>Berry</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+export default Food
